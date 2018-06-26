f026bfdd2efec2f94f352e0db2436040f8e0b0ae
= <ekirapapaul@gmail.com>
2018-06-26 11:56:15 +0300

    [ADD] tests dependencies

--- api/src/main/java/org/openmrs/module/wellnessinventory/WellnessinventoryActivator.java
+++ api/src/main/java/org/openmrs/module/wellnessinventory/WellnessinventoryActivator.java
@@ -11,6 +11,7 @@ package org.openmrs.module.wellnessinventory;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.openmrs.api.context.Context;
 import org.openmrs.module.BaseModuleActivator;
 
 /**
